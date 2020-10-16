b14dafda255613ab4b5b5c223cfeeaba6eb7b15f
Koen De Jans <dejanskoen@gmail.com>
2020-10-16 09:11:51 +0200

    Add project files.

---
+++ ProjectKoenDeJansDataBase/Login.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace ProjectKoenDeJansDataBase
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        static String connectionString = @"Data Source=192.168.0.192;Initial Catalog=DBCustomer;User ID=sa;Password=1234;";
+
+        public  Login()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click_1(object sender, RoutedEventArgs e)
+        {
+            String message = "Invalid Credentials";
+            try
+            {
+                con = new SqlConnection(connectionString);
+                con.Open();
+                cmd = new SqlCommand("Select * from tblCustomerInfo where CustomerEmail=@CustomerEmail", con);
+                cmd.Parameters.AddWithValue("@CustomerEmail", txtUserId.Text.ToString());
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    if (reader["Password"].ToString().Equals(txtPassword.Password.ToString(), StringComparison.InvariantCulture))
+                    {
+                        message = "1";
+                        UserInfo.CustomerEmail = txtUserId.Text.ToString();
+                        UserInfo.CustomerName = reader["CustomerName"].ToString();
+                    }
+                }
+
+                reader.Close();
+                reader.Dispose();
+                cmd.Dispose();
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message.ToString();
+            }
+            if (message == "1")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show(message, "Info");
+        }
+
+        private void btnClose_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+    
+
---
+++ ProjectKoenDeJansDataBase/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectKoenDeJansDataBase
+{
+    public class UserInfo
+    {
+        public static String CustomerName = "";
+        public static String CustomerEmail = "";
+    }
+}
